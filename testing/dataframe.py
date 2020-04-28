0ee36ce62c248c244feea773e17aa15091730976
Matsuoka Kota <kota11250910@gmail.com>
2020-04-28 23:01:42 +0900

    feat: add checking columns

--- testing/dataframe.py
+++ testing/dataframe.py
@@ -1,14 +1,23 @@
 from pyspark.sql.dataframe import DataFrame
 
 
-def assert_dataframe_equal(left, right):
+def assert_dataframe_equal(left, right, columns_order=True):
     """
     Check that left and right are equal.
 
     :param left: DataFrame
     :param right: DataFrame
+    :param columns_order: bool
 
     - Check left and right are DataFrame instance
+    - Check equal columns
     """
     assert isinstance(left, DataFrame), "left is not an DataFrame"
     assert isinstance(right, DataFrame), "right is not an DataFrame"
+
+    if columns_order:
+        assert left.columns == right.columns, "columns are not equal"
+    else:
+        assert left.columns == right.select(left.columns).columns, "columns are not equal"
+
+
--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -1,13 +1,19 @@
-from testing.dataframe import assert_dataframe_equal
-from pyspark.sql import SparkSession
+import pandas as pd
 import pytest
+from pyspark.sql import SparkSession
+
+from testing.dataframe import assert_dataframe_equal
 
 spark = SparkSession.builder.getOrCreate()
 
 
 @pytest.fixture
 def data():
-    return [("test", 1)]
+    return pd.DataFrame(
+        data=[['a', 'b', 'c'],
+              ['x', 'y', 'z']],
+        columns=['col_1', 'col_2', 'col_3']
+    )
 
 
 def test_assert_frame_equal(data):
@@ -22,4 +28,27 @@ def test_is_no_dataframe_instance(data):
     right = dict()
 
     with pytest.raises(AssertionError, match="right is not an DataFrame"):
+        assert_dataframe_equal(left, right)
+
+
+def test_is_out_of_order_columns(data):
+    second_data = pd.DataFrame(
+        data=[['x', 'y', 'z']],
+        columns=['col_3', 'col_2', 'col_1']
+    )
+    left = spark.createDataFrame(data)
+    right = spark.createDataFrame(second_data)
+
+    assert_dataframe_equal(left, right, columns_order=False)
+
+
+def test_is_not_equal_columns(data):
+    second_data = pd.DataFrame(
+        data=[['x', 'y', 'z']],
+        columns=['col_x', 'col_y', 'col_z']
+    )
+    left = spark.createDataFrame(data)
+    right = spark.createDataFrame(second_data)
+
+    with pytest.raises(AssertionError, match="columns are not equal"):
         assert_dataframe_equal(left, right)
